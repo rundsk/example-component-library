934bdf324c2c48f855ba0dd530f45bba8f89daf9
Marius Wilms <mariuswilms@mailbox.org>
2019-11-01 11:12:22 +0100

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2020 Marius Wilms All rights reserved.
+ * Copyright 2019 Atelier Disko. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  plugins: [
+    resolve({
+      browser: true,
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+};
+
---
+++ src/Button/index.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2020 Marius Wilms All rights reserved.
+ * Copyright 2019 Atelier Disko. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+function Button(props) {
+  return (
+    <button>Hello DSK!</button>
+  );
+}
+
+export default Button;
+
